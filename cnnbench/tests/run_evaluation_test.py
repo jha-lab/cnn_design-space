bc6e61dfe1ed692eaef5ef4653c8c11df6f4561f
shikhartuli <shikhartuli98@gmail.com>
2021-01-22 11:26:44 -0500

    Updated tests

--- cnnbench/tests/run_evaluation_test.py
+++ cnnbench/tests/run_evaluation_test.py
@@ -53,22 +53,24 @@ FLAGS(sys.argv)
 
 def test_basic_model_run():
   """Testing basic model run for two vertices and one repeat"""
-  # # Takes a lot of time to run
+  # Takes a lot of time to run
 
-  # warnings.filterwarnings("ignore")
+  warnings.filterwarnings("ignore")
 
-  # FLAGS.module_vertices = 2
-  # FLAGS.num_repeats = 1
-  # FLAGS.output_file = './results/vertices_2_test/generated_graphs.json'
-  # FLAGS.models_file = './results/vertices_2_test/generated_graphs.json' 
-  # FLAGS.output_dir = './results/vertices_2_test/evaluation' 
-  # FLAGS.cleanup_dir = './results/vertices_2_test/evaluation'
-  # FLAGS.model_dir = './results/vertices_2_test'
-  # FLAGS.data_dir = './datasets_test'
+  FLAGS.module_vertices = 2
+  FLAGS.num_repeats = 1
+  FLAGS.num_stacks = 1
+  FLAGS.num_modules_per_stack = 1
+  FLAGS.run_nasbench = True
+  FLAGS.output_file = './results/vertices_2_test/generated_graphs.json'
+  FLAGS.models_file = './results/vertices_2_test/generated_graphs.json' 
+  FLAGS.output_dir = './results/vertices_2_test/evaluation' 
+  FLAGS.cleanup_dir = './results/vertices_2_test/evaluation'
+  FLAGS.model_dir = './results/vertices_2_test'
+  FLAGS.data_dir = './datasets_test'
   
-  # generate_tfrecords.main('cifar10', FLAGS.data_dir, None)
-  # generate_graphs_script.main(1)
-  # run_evaluation_script.main(1)
-  # # cleanup_script.main(1) Taken into account by Github Action
+  generate_tfrecords.main('cifar10', FLAGS.data_dir, None)
+  generate_graphs_script.main(1)
+  run_evaluation_script.main(1)
+  # cleanup_script.main(1) Taken into account by Github Action
 
-  assert True
