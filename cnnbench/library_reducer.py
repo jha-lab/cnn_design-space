297c98b462f8539a6a8c18485c9f86b132da14a8
shikhartuli <shikhartuli98@gmail.com>
2021-12-27 14:58:50 -0500

    Bug fixes

--- cnnbench/library_reducer.py
+++ cnnbench/library_reducer.py
@@ -16,24 +16,28 @@ MODULES_SAMPLE_SIZE = 50
 HEADS_SAMPLE_SIZE = 50
 
 
-if os.path.exists(CKPT_TEMP):
-	print('Loading checkpoint...')
-	ckpt = pickle.load(open(CKPT_TEMP, 'rb'))
+def main():
+	if os.path.exists(CKPT_TEMP):
+		print('Loading checkpoint...')
+		ckpt = pickle.load(open(CKPT_TEMP, 'rb'))
 
-	print('Reducing library...')
-	sampled_keys = sample(ckpt['module_buckets'].keys(), MODULES_SAMPLE_SIZE)
-	ckpt['module_buckets'] = {key: ckpt['module_buckets'][key] for key in sampled_keys}
-	ckpt['total_modules'] = len(ckpt['module_buckets'])
+		print('Reducing library...')
+		sampled_keys = sample(ckpt['module_buckets'].keys(), MODULES_SAMPLE_SIZE)
+		ckpt['module_buckets'] = {key: ckpt['module_buckets'][key] for key in sampled_keys}
+		ckpt['total_modules'] = len(ckpt['module_buckets'])
 
-	sampled_keys = sample(ckpt['head_buckets'].keys(), HEADS_SAMPLE_SIZE)
-	ckpt['head_buckets'] = {key: ckpt['head_buckets'][key] for key in sampled_keys}
-	ckpt['total_heads'] = len(ckpt['head_buckets'])
+		sampled_keys = sample(ckpt['head_buckets'].keys(), HEADS_SAMPLE_SIZE)
+		ckpt['head_buckets'] = {key: ckpt['head_buckets'][key] for key in sampled_keys}
+		ckpt['total_heads'] = len(ckpt['head_buckets'])
 
-	ckpt['total_graphs'], ckpt['stacks_done'], ckpt['graph_buckets'] = 0, 0, {}
+		ckpt['total_graphs'], ckpt['stacks_done'], ckpt['graph_buckets'] = 0, 0, {}
 
-	print('Saving reduced library...')
-	pickle.dump(ckpt, open(CKPT_TEMP, 'wb+'), pickle.HIGHEST_PROTOCOL)
+		print('Saving reduced library...')
+		pickle.dump(ckpt, open(CKPT_TEMP, 'wb+'), pickle.HIGHEST_PROTOCOL)
 
-	print('Done!')
-else:
-	print('Checkpoint not found!')
+		print('Done!')
+	else:
+		print('Checkpoint not found!')
+
+if __name__ == '__main__':
+    main()
